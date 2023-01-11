aa17b0d34d5ce67639d809c5fdcd46b55a2cbff3
Gratien1 <gratiensossouzin1@gmail.com>
2023-01-11 14:58:29 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,17 +1,10 @@
-q_add(my_list=[]):
-
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
     res_list = []
-
     res = 0
-
     for item in my_list:
-
         if item not in res_list:
-
             res_list.append(item)
-
     for uniqs in res_list:
-
         res += uniqs
-
     return res
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,4 @@
-dhjjhg
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
