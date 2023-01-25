1030d89ee17d8415c310520552ae8ea83455d06f
Gratien1 <gratiensossouzin1@gmail.com>
2023-01-25 01:37:09 +0000

    0x05-python-exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,12 @@
-fdjfgdh
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except BaseException as e:
+        res = None
+        print("Exception: {}".format(e), file=sys.stderr)
+    finally:
+        return res
