1bb2267985ffde0641f66c6ae667a1cfd93d29b0
Gratien1 <gratiensossouzin1@gmail.com>
2023-01-31 01:05:23 +0000

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,1 +1,9 @@
-gchgd
+#!/usr/bin/python3
+"""0-rectangle, built for Holberton project 0x08 task 0.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
