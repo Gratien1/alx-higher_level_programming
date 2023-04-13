25687e1b7c7e34336b84d26a77a7dd5347437da9
Gratien1 <gratiensossouzin1@gmail.com>
2023-04-13 03:03:45 +0100

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/tests/4-main.js
+++ 0x13-javascript_objects_scopes_closures/tests/4-main.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('../4-rectangle');
+const Rectangle = require('./4-rectangle');
 
 const r1 = new Rectangle(2, 3);
 console.log('Normal:');
@@ -7,8 +7,4 @@ r1.print();
 
 console.log('Double:');
 r1.double();
-r1.print();
-
-console.log('Rotate:');
-r1.rotate();
 r1.print();
