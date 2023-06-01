cb497dd2ce8182ebc8cb60f132f59e564fbff511
Gratien <gratiensossouzin1@gmail.com>
2023-06-01 01:00:09 +0100

    Javascript - Web JQuery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+$(() => {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json', (data, textStatus) => {
+      if (textStatus === 'success') {
+        const films = data.results;
+        films.forEach(film => {
+          $('#list_movies').append('<li>' + film.title + '</li>');
+        });
+      }
+    });
+  });
